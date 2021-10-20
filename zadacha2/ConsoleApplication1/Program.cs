cd5b15e226970ebe1890078a049b883622a1d3d8
Nikmoz1 <49882944+Nikmoz1@users.noreply.github.com>
2021-10-20 15:35:46 +0300

    z1z2

--- zadacha2/ConsoleApplication1/Program.cs
+++ zadacha2/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication4
 {
     class peoem
     {
+        public static int count_of_objects = 0;
+        public static int ram_filter;
         private string processor;
         private int frequency;
         private int ram;
@@ -20,6 +22,7 @@ namespace ConsoleApplication4
             frequency = _frequency;
             ram = _ram;
             type = _type;
+            count_of_objects++;
         }
         public string Processor
         {
@@ -53,17 +56,51 @@ namespace ConsoleApplication4
             get { return type; }
             set { type = Regex.Replace(value, @"[^A-zА-я]+", String.Empty); }
         }
-        public void Display()
+
+        public void display_info()
         {
             Console.WriteLine($"Процесор: {processor}\nЧастота: {frequency}\nRam: {ram}\nТип: {type}");
+        }
+        public bool filter()
+        {
+            if (ram > peoem.ram_filter)
+                return true;
+            else
+                return false;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            peoem Peoem = new peoem("Pentium-III", 233, 4, "C");
-            Peoem.Display();
+            peoem[] Peoem = new peoem[10];
+            Peoem[0] = new peoem("1", 321, 532, "qwe");
+            Peoem[0].display_info();
+            Peoem[1] = new peoem("2", 213, 12, "qwd");
+            Peoem[1].display_info();
+            Peoem[2] = new peoem("3", 213, 122, "qwe");
+            Peoem[2].display_info();
+            Peoem[3] = new peoem("4", 56, 321, "sad");
+            Peoem[3].display_info();
+            Peoem[4] = new peoem("5", 12,32, "asd");
+            Peoem[4].display_info();
+            Peoem[5] = new peoem("6", 432, 54, "asd");
+            Peoem[5].display_info();
+            Peoem[6] = new peoem("7", 123, 435, "dsa");
+            Peoem[6].display_info();
+            Peoem[7] = new peoem("8", 12, 234, "ad");
+            Peoem[7].display_info();
+            Peoem[8] = new peoem("9", 123, 45, "sd");
+            Peoem[8].display_info();
+            Peoem[9] = new peoem("10", 123, 412, "sad");
+            Peoem[9].display_info();
+            Console.WriteLine("Кiлькiсть об'єктiв в класi: " + peoem.count_of_objects);
+            Console.WriteLine("\nВведiть мiнiмальне значення RAM для пошуку за критерiєм: ");
+            peoem.ram_filter = int.Parse(Console.ReadLine());
+            Console.WriteLine("Об'єкти, RAM яких бiльшe, нiж " + peoem.ram_filter + ": \n");
+            for (int i = 0; i < peoem.count_of_objects; i++)
+                if (Peoem[i].filter())
+                    Peoem[i].display_info();
         }
     }
 }
