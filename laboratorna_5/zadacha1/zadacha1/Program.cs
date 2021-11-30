0ac98abca2a82ae6dc9ca0195fd07d699034adda
Nikmoz1 <mmi200212@gmail.com>
2021-11-30 14:58:04 +0200

    add lab_5

---
+++ laboratorna_5/zadacha1/zadacha1/Program.cs
@@ -0,0 +1,67 @@
+﻿interface IShape
+{
+    string sizeFigure { get; }
+    string Type();
+    string Square();
+    string Length();
+}
+interface IColoredShape : IShape
+{
+    string name { get; }
+}
+class Rectangle : IShape
+{
+    protected double a { get; set; }
+    protected double b { get; set; }
+    public Rectangle(double A, double B)
+    {
+        this.a = A;
+        this.b = B;
+    }
+    public string sizeFigure
+    {
+        get
+        {
+            return $"Розмiр фiгури: A = {a}, B = {b}";
+        }
+    }
+    public string Length()
+    {
+        double d = Math.Round(Math.Sqrt(Math.Pow(b, 2) + Math.Pow(a, 2)), 2);
+        return $"Довжина дiагоналi фiгури = {d}";
+    }
+    public string Square()
+    {
+        double s = Math.Round(a * b, 2);
+        return $"Площа фiгури = {s}";
+    }
+    public string Type()
+    {
+        return "Тип: прямокутник";
+    }
+}
+class ColoredShape : Rectangle, IColoredShape
+{
+    protected string color { get; set; }
+    public string name { get; set; }
+    public ColoredShape(double A, double B, string C) : base(A, B)
+    {
+        this.color = C;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle[] rectangle = {
+         new Rectangle (2.56, 4.22),
+         new Rectangle (6.15, 2.5),
+         new Rectangle (2.5, 4.55)
+         };
+        foreach (Rectangle str in rectangle)
+        {
+
+            Console.WriteLine($"{str.Type()}\n{str.sizeFigure}\n{str.Square()}\n{str.Length()}");
+        }
+    }
+}
---
+++ laboratorna_5/zadacha2/zadacha2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace exampleone
+{
+
+	public class Allthatprints
+	{
+
+		public static void Main(string[] args)
+		{
+
+			Book book1 = new Book();
+			Book book2 = new Book();
+			Book book3 = new Book();
+			Book book4 = new Book();
+			Magazine magazine = new Magazine();
+			Magazine magazine2 = new Magazine();
+			Magazine magazine3 = new Magazine();
+			Magazine magazine4 = new Magazine();
+
+			Printable[] printables = new Printable[] { book1, book2, book3, book4, magazine, magazine2, magazine3, magazine4 };
+
+
+			for (int i = 0; i < printables.Length; i++)
+			{
+
+				if (printables[i] is Book)
+				{
+					Book.printBook(printables);
+				}
+				else
+				{
+					Magazine.printMagazines(printables);
+				}
+			}
+		}
+	}
+	public class Book : Printable
+	{
+
+		public virtual void print()
+		{
+			Console.WriteLine("The book is printed!");
+		}
+
+		internal static void printBook(Printable[] printable)
+		{
+			Console.WriteLine("Title of the Book!" + printable.ToString());
+		}
+
+	}
+	public class Magazine : Printable
+	{
+		public virtual void print()
+		{
+			Console.WriteLine("The Magazine is printed!");
+		}
+
+		internal static void printMagazines(Printable[] printable)
+		{
+			Console.WriteLine("Title of the Magazine!" + printable.ToString());
+		}
+
+	}
+	public interface Printable
+	{
+
+		void print();
+	}
+}
+
+
