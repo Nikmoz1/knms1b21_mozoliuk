a194f8a673dec86145ed3d7bc356d4d86077fa71
Nikmoz1 <mmi200212@gmail.com>
2021-12-01 16:04:50 +0200

    add task2

--- mkr/mkr2/mkr2/Program.cs
+++ mkr/mkr2/mkr2/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿interface ICare
+{
+    string Car { get; }
+    string Potuzhnist();
+}
+
+class Audi : ICare
+{
+    protected string marka { get; set; }
+
+    protected string kyzov { get; set; }
+
+    protected double rik { get; set; }
+    public Audi(string marka, string kyzov, int rik)
+    {
+        this.marka = marka;
+        this.kyzov = kyzov;
+        this.rik = rik;
+    }
+    public string Car
+    {
+        get
+        {
+            return $"Марка: {marka}, Кузов: {kyzov}, Рік: {rik}";
+        }
+    }
+    public string Potuzhnist()
+    {
+        switch (rik)
+        {
+            case 1998:
+                Console.WriteLine("80 кiнських сил");
+                break;
+            case 2004:
+                Console.WriteLine("130 кiнських сил");
+                break;
+            case 2007:
+                Console.WriteLine("140 кiнських сил");
+                break;
+
+        }
+        return $"{marka},{kyzov}, {rik}";
+
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Audi[] audi = {
+             new Audi ("a6", "седан", 2004),
+             new Audi ("a6", "седан", 2007),
+             new Audi ("a6", "ceдан", 1998)
+    };
+        foreach (Audi str in audi)
+        {
+
+           Console.WriteLine($"{str.Potuzhnist()}");
+        }
+    }
+}
